0ba19d5bd4db0f3d6b271b9363a7a0ebaca6aba1
jedi2610 <krithickumarub@protonmail.com>
2021-04-04 12:56:04 +0530

    added spotify script

--- config.h
+++ config.h
@@ -1,14 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Command*/						/*Update Interval*/			/*Update Signal*/
-	{"dbdiskusage",					120,						15},
+	{"dbspotify",					5,						8},
+	{"dbdiskusage",					300,						9},
 	{"dbvolume",					0,							10},
 	{"dbmemusage",					10,							11},
-	{"dbcpuusage",					10,							12},
+	{"dbcpuusage",					10,							12}, /* costly, so i run it only every 10 secs */
 	{"dbcputemp",					5,							13},
 	{"dbdate",						60,							14},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "  |  ";
+/* sets delimeter between status commands. NULL character ('\0') means no delimeter. */
+static char delim[] = "  ";
 static unsigned int delimLen = 5;
